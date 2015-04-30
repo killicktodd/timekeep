a7021e4d3c180c588268300f70c69905914ea13c
Todd Killick <killicktodd@yahoo.co.uk>
2015-04-30 14:35:17 +0100

    Currency in £

--- app/models/watch.rb
+++ app/models/watch.rb
@@ -3,4 +3,8 @@ class Watch < ActiveRecord::Base
 	validates :description, presence: true
 	validates :brand, presence: true
 	validates :price_in_pence, numericality: {greater_than_or_equal_to:3999}
+
+	def price
+		price_in_pence.to_f / 100
+	end
 end
