0dd710712ff6d43a7f67c9588ff2aba788b336b3
Todd Killick <killicktodd@yahoo.co.uk>
2015-04-30 16:46:26 +0100

    Genereate model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,40 @@
 class UsersController < ApplicationController
   def new
-  	user = User.new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		# Log the user in
+  		# Wipe the session clean and then log new user in
+  		reset_session
+  		session[:user_id] = @user.id
+  		flash[:success] = "Thanks for signing up"
+  		redirect_to root_path
+  	else
+  		flash[:error] = "Please check the form for errors and try again"
+  		render :new
+  	end
   end
 
   def edit
+  end
+
+  private
+  def user_params
+  	params.require(:user).permit(
+  		:first_name, 
+  		:last_name, 
+  		:password, 
+  		:password_confirmation, 
+  		:username,
+  		:email,
+  		:address_line_one,
+  		:address_line_two,
+  		:address_line_three,
+  		:address_line_four,
+  		:postcode
+  		)
   end
 end
