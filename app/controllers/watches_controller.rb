c777b47c431205afc9d6a0223eb21b160894fad8
Todd Killick <killicktodd@yahoo.co.uk>
2015-04-30 11:14:43 +0100

    Index markup

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -1,5 +1,10 @@
 class WatchesController < ApplicationController
   def index
+  	@watches = [{name: "Backup Black", description: "Backup Black Lorem ipsum", price_in_pence: 50000},
+  				{name: "Classic Digital Casio", description: "Casio Lorem ipsum", price_in_pence: 4999},
+  				{name: "1960s Vintage Raketa", description: "1960s Lorem ipsum", price_in_pence: 12000},
+  				{name: "Max Bill Chronoscope", description: "Max Bill Lorem ipsum", price_in_pence: 200000}
+  				]
   end
 
   def show
@@ -8,6 +13,15 @@ class WatchesController < ApplicationController
   def new
   end
 
+  def create
+  end
+
   def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
