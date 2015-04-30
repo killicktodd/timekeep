ca76a197bdf73ccde0a838b95f4fc90c5f63715b
Todd Killick <killicktodd@yahoo.co.uk>
2015-04-30 17:08:33 +0100

    Logged_in? helper and name methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :current_user, :logged_in?
+
   def current_user
-  	if session[:user_id].present?
-  		User.find(session[user:id])
+  	if logged_in?
+  		User.find(session[:user_id])
   	end
   end
+
+  def logged_in?
+  	session[:user_id].present?
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ActiveRecord::Base
 	has_secure_password
+
+	validates :email, presence: true
+
+	def full_name
+		if first_name.present? and last_name.present?
+			[first_name, last_name].join(" ")
+		else name
+		end
+	end
+
+	def name
+		first_name || username || email
+	end
 end
