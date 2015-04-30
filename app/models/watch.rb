55371e55efa9080ff6dd442f94701b6a842dc295
Todd Killick <killicktodd@yahoo.co.uk>
2015-04-30 13:01:09 +0100

    Validates watches

--- app/models/watch.rb
+++ app/models/watch.rb
@@ -1,2 +1,6 @@
 class Watch < ActiveRecord::Base
+	validates :name, presence: true, uniqueness: true
+	validates :description, presence: true
+	validates :brand, presence: true
+	validates :price_in_pence, numericality: {greater_than_or_equal_to:3999}
 end
