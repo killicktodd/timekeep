e8a23796b1f2aa2a99765e6acd58cd75edc5f0e5
Todd Killick <killicktodd@yahoo.co.uk>
2015-05-01 15:12:48 +0100

    Stripe charge code

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,15 @@ class Order < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :watch
 	validates :stripe_token, presence: true
+
+	def save_and_charge
+		Stripe.api_key = Rails.application.secrets.stripe_secret_key
+
+		Stripe::Charge.create(
+		  :amount => 400,
+		  :currency => "gbp",
+		  :source => "tok_15xeXRDdG45x16nLgJrvol1Y", # obtained with Stripe.js
+		  :description => "Charge for test@example.com"
+		)	
+	end
 end
