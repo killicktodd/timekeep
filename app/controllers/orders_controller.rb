b450ded4e330ecae6b704dabcbb382e3736cc601
Todd Killick <killicktodd@yahoo.co.uk>
2015-05-01 12:41:43 +0100

    Allowing orders

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,34 @@
+class OrdersController < ApplicationController
+  before_action :require_user, except [:show]
+
+  def new
+  	@order = Order.new
+  end
+
+  def create
+  	# Find the watch
+  	@watch = Watch.find(params[watch_id])
+  	# Create a new order with watch_id = @watch.id
+  	@order = @watch.orders.new(order_params)
+  	# Set user_id = current_user_id
+  	@order.user = current_user
+  	# Charging code goes here
+  	if @order.save
+  		flash[:success] = "Success"
+  		redirect_to @order
+  	else
+  		flash[:error] = "Please check form for errors and try again"
+  		render :new
+  end
+
+  def show
+  	@order = Order.find(params[:id])
+  	require_owner(@order) # Make sure order.user_id == curent_user.id
+  end
+
+  private
+  def order_params
+  	params.require(:order).perm(:stripe_token)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'orders/new'
+
+  get 'orders/show'
+
 	root "watches#index"
 	resources :watches
 	resources :users, except: [:index, :show]
